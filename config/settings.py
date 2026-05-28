5c9a431f9d21b432e9b552dfcb52da087abdc8e4
aboorvang-96 <aboorvang96@gmail.com>
2026-05-28 21:52:38 +0530

    Fix Railway database env fallback

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME', default='postgres'),
         'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
     }
